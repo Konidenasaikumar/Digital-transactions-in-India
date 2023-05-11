5daad800b67282baa3a23ba95d3adc9f0f94d006
Sai Kumar Konidena <117543948+Konidenasaikumar@users.noreply.github.com>
2023-05-12 00:42:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -77,14 +77,14 @@ selected_df = aggr_states_trans[(aggr_states_trans['Quarter'] == quarter_choice)
 # Creating the map
 
 
-fig2=px.choropleth(selected_df,
-                    locations= 'state_code', 
+fig2= px.choropleth(selected_df, 
+                    locations= 'States', 
                     geojson= 'https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson', 
-                    featureidkey= 'properties.state_code',
+                    featureidkey= 'properties.ST_NM',
                     color= 'Amount in Millions',
                     scope = 'asia', 
                     hover_name= 'States',
-                    hover_data= [ "Transaction_count", "Amount in Millions"],  
+                    hover_data= [ "Transaction_count", "Amount in Millions"],
                     color_continuous_scale='Viridis')
 
  
