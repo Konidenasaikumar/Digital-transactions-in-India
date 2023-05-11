9735920a1cf6adb10321a39bc80131b2c255e2f8
Sai Kumar Konidena <117543948+Konidenasaikumar@users.noreply.github.com>
2023-05-12 01:01:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ fig1=(px.choropleth(selected_df1,
             color= 'Amount in Millions',
             color_continuous_scale='viridis'))
 fig1.update_geos(fitbounds= "locations", visible= False)
-#fig1.update_layout( height=600, width=800)
         
 with col1:
     st.plotly_chart(fig1)
@@ -81,9 +80,6 @@ with col1:
                     hover_name= 'States',
                     hover_data= [ "Transaction_count", "Amount in Millions"],
                     color_continuous_scale='Viridis')
-
-    fig2.update_layout( height=600, width=800)
-    fig2.update_layout(mapbox_style="open-street-map")
     fig2.update_geos(fitbounds= "locations", visible= False)
     st.plotly_chart(fig2, use_container_width=True)
 
